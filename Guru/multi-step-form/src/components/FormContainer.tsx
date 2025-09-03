c47e7b6afae2592b38953b88c2832b0421cb258b
Maria Sidorova <sidmashav@icloud.com>
2025-09-03 13:23:36 +0100

    Fixed ReadMe

--- Guru/multi-step-form/src/components/FormContainer.tsx
+++ Guru/multi-step-form/src/components/FormContainer.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Stepper from './Stepper'
-import Forms from './Forms'
+import React from "react";
+import Stepper from "./Stepper";
+import Forms from "./Forms";
 
 const FormContainer = () => {
   return (
     <section className="relative flex w-full flex-col rounded-2xl shadow-md lg:min-h-[600px] lg:max-w-4xl lg:flex-row lg:bg-white lg:p-4">
-    <Stepper/>
-    <div className="w-full lg:h-auto lg:px-16 lg:py-10">
-      <Forms/>
-    </div>
-  </section>
-  )
-}
+      <Stepper />
+      <div className="w-full lg:h-auto lg:px-16 lg:py-10">
+        <Forms />
+      </div>
+    </section>
+  );
+};
 
-export default FormContainer
+export default FormContainer;
--- Guru/multi-step-form/src/forms/FinishingUpForm.tsx
+++ Guru/multi-step-form/src/forms/FinishingUpForm.tsx
@@ -5,94 +5,84 @@ import StepBtns from "../components/StepBtns";
 import ThankYou from "../components/ThankYou";
 
 const FinishingUpForm = () => {
-	const { formData, decreaseStep, isConfirmed } = useStepStore();
-	const calculateTotal = () => {
-		let total = 0;
-		total += formData.price;
-		total = formData.addOns.reduce(
-			(accumulator, item) => accumulator + item.price,
-			total
-		);
-		return total;
-	};
-	return (
-		<div className="relative -top-9 mx-auto h-full w-[90%] rounded-lg bg-white px-6 py-8 shadow-md lg:top-0 lg:mx-0 lg:w-auto lg:bg-none lg:p-0 lg:shadow-none">
-			{isConfirmed ? (
-				<ThankYou />
-			) : (
-				<div className="flex size-full flex-col">
-					<FormTitle
-						title="Finishing Up"
-						description="Double-check everything looks OK before confirming."
-					/>
-					<div className="mt-5 flex size-full flex-col gap-6 lg:mt-10">
-						<div className="flex flex-col gap-6">
-							<div className="bg-form-very-light-grey rounded-md px-6 py-4">
-								<div className="flex items-center justify-between">
-									<div className="flex flex-col gap-1.5">
-										<span className="text-form-denim text-regular font-medium capitalize">
-											{`${formData?.plan} (${formData?.billingTime})`}
-										</span>
-										<span
-											onClick={() => decreaseStep(2)}
-											className="text-form-grey hover:text-form-purple cursor-pointer text-sm underline transition-colors"
-										>
-											Change
-										</span>
-									</div>
-									<div>
-										<span className="text-regular text-form-denim font-bold">
-											{"$" + formData?.price + "/"}
-											{formData?.billingTime === "monthly"
-												? "mo"
-												: "yr"}
-										</span>
-									</div>
-								</div>
-								<div className="bg-form-light-grey mt-6 h-[1px] w-full" />
-								<div className="mt-4 flex flex-col gap-4">
-									{formData.addOns.map((item) => (
-										<div
-											key={item.addOn}
-											className="flex items-center justify-between"
-										>
-											<span className="text-form-grey text-sm">
-												{item.addOn}
-											</span>
-											<span className="text-form-denim text-sm">
-												{"+" + "$" + item.price + "/"}
-												{formData?.billingTime ===
-												"monthly"
-													? "mo"
-													: "yr"}
-											</span>
-										</div>
-									))}
-								</div>
-							</div>
-						</div>
-						<div className="flex items-center justify-between px-6">
-							<span className="text-regular text-form-grey">
-								Total(per{" "}
-								{formData?.billingTime === "monthly"
-									? "month"
-									: "year"}
-								)
-							</span>
+  const { formData, decreaseStep, isConfirmed } = useStepStore();
+  const calculateTotal = () => {
+    let total = 0;
+    total += formData.price;
+    total = formData.addOns.reduce(
+      (accumulator, item) => accumulator + item.price,
+      total
+    );
+    return total;
+  };
+  return (
+    <div className="relative -top-9 mx-auto h-full w-[90%] rounded-lg bg-white px-6 py-8 shadow-md lg:top-0 lg:mx-0 lg:w-auto lg:bg-none lg:p-0 lg:shadow-none">
+      {isConfirmed ? (
+        <ThankYou />
+      ) : (
+        <div className="flex size-full flex-col">
+          <FormTitle
+            title="Finishing Up"
+            description="Double-check everything looks OK before confirming."
+          />
+          <div className="mt-5 flex size-full flex-col gap-6 lg:mt-10">
+            <div className="flex flex-col gap-6">
+              <div className="bg-form-very-light-grey rounded-md px-6 py-4">
+                <div className="flex items-center justify-between">
+                  <div className="flex flex-col gap-1.5">
+                    <span className="text-form-denim text-regular font-medium capitalize">
+                      {`${formData?.plan} (${formData?.billingTime})`}
+                    </span>
+                    <span
+                      onClick={() => decreaseStep(2)}
+                      className="text-form-grey hover:text-form-purple cursor-pointer text-sm underline transition-colors"
+                    >
+                      Change
+                    </span>
+                  </div>
+                  <div>
+                    <span className="text-regular text-form-denim font-bold">
+                      {"$" + formData?.price + "/"}
+                      {formData?.billingTime === "monthly" ? "mo" : "yr"}
+                    </span>
+                  </div>
+                </div>
+                <div className="bg-form-light-grey mt-6 h-[1px] w-full" />
+                <div className="mt-4 flex flex-col gap-4">
+                  {formData.addOns.map((item) => (
+                    <div
+                      key={item.addOn}
+                      className="flex items-center justify-between"
+                    >
+                      <span className="text-form-grey text-sm">
+                        {item.addOn}
+                      </span>
+                      <span className="text-form-denim text-sm">
+                        {"+" + "$" + item.price + "/"}
+                        {formData?.billingTime === "monthly" ? "mo" : "yr"}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-between px-6">
+              <span className="text-regular text-form-grey">
+                Total(per{" "}
+                {formData?.billingTime === "monthly" ? "month" : "year"})
+              </span>
 
-							<span className="text-form-purple text-xl font-bold">
-								{"+" + "$" + calculateTotal() + "/"}
-								{formData?.billingTime === "monthly"
-									? "mo"
-									: "yr"}
-							</span>
-						</div>
-					</div>
-				</div>
-			)}
-			<StepBtns />
-		</div>
-	);
+              <span className="text-form-purple text-xl font-bold">
+                {"+" + "$" + calculateTotal() + "/"}
+                {formData?.billingTime === "monthly" ? "mo" : "yr"}
+              </span>
+            </div>
+          </div>
+        </div>
+      )}
+      <StepBtns />
+    </div>
+  );
 };
 
 export default FinishingUpForm;
